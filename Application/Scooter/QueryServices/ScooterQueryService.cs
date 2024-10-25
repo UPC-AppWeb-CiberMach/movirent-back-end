dd9938408b0299a73fbe34767eafa436e31615f5
MesSobble <u201911727@upc.edu.pe>
2024-10-24 20:04:37 -0500

    feat: add scooter in application

---
+++ Application/Scooter/QueryServices/ScooterQueryService.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Scooter.Model.Queries;
+using Domain.Scooter.Repositories;
+using Domain.Scooter.Services;
+using Domain.Scooter.Model.Entities;
+
+namespace Application.Scooter.QueryServices;
+
+public class ScooterQueryService : IScooterQueryService
+{
+    private readonly IScooterRepository _scooterRepository;
+
+    public ScooterQueryService(IScooterRepository scooterRepository)
+    {
+        _scooterRepository = scooterRepository;
+    }
+
+    public async Task<IEnumerable<Scooter>?> Handle(GetAllScootersQuery query)
+    {
+        return await _scooterRepository.ListAsync();
+    }
+
+    public Task<List<Scooter>?> Handle(GetAllScootersQuery query)
+    {
+        throw new NotImplementedException();
+    }
+}
