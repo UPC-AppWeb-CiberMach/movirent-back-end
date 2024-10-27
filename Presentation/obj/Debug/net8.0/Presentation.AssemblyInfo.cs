9954fcd42da9a52904ca11668f2d287c6d1465c3
MesSobble <u201911727@upc.edu.pe>
2024-10-27 02:41:27 -0500

    feat: add dependencies

--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+275ee351b60479aa2f22bfeeaa2147e0a2514bab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56e5a69992c3dfce49b0ec1d744752f893ae5a35")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
