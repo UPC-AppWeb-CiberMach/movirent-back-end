429211eea26074e729040162a631cc9e7db70b75
QuiqueVladimir <U202022365@upc.edu.pe>
2024-10-29 21:06:11 -0500

    update: added ReservationRepository with methods to check scooter availability and user active reservations

---
+++ Infrastructure/Reservation/ReservationRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Reservation.Model.Entities;
+using Domain.Reservation.Repositories;
+using Infrastructure.Shared.Persistence.EFC.Configuration;
+using Infrastructure.Shared.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Reservation;
+
+public class ReservationRepository(AppDbContext context) : BaseRepository<ReservationEntity>(context), IReservationRepository
+{
+    public async Task<ReservationEntity?> CheckScooterAvailableAsync(int id)
+    {
+        return await context.Reservations.Where(r => r.ScooterId == id).FirstOrDefaultAsync();
+    }
+    public async Task<ReservationEntity?> CheckUserActiveReservationAsync(int id)
+    {
+        return await context.Reservations.Where(r => r.UserId == id).FirstOrDefaultAsync();
+    }
+    
+}
