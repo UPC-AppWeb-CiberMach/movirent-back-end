fd1cababa4ee502a573d3549c6bc91adf7ccf18f
QuiqueVladimir <U202022365@upc.edu.pe>
2024-10-29 20:46:09 -0500

    update: added methods async to Update and Remove

--- Domain/Shared/IBaseRepository.cs
+++ Domain/Shared/IBaseRepository.cs
@@ -5,6 +5,8 @@ public interface IBaseRepository<TEntity> where TEntity : class
     Task<IEnumerable<TEntity>> GetAllAsync();
     Task<TEntity?> GetByIdAsync(int id);
     Task<TEntity> AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task RemoveAsync(TEntity entity);
     void Update(TEntity entity);
     void Delete(TEntity entity);
 }
