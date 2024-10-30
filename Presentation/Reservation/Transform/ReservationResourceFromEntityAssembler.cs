33eb4b77cc9d64371ab2bbd63e143e8ababd55cc
QuiqueVladimir <U202022365@upc.edu.pe>
2024-10-29 21:08:16 -0500

    update: added ReservationResourceFromEntityAssembler to transform reservation entities to resources

---
+++ Presentation/Reservation/Transform/ReservationResourceFromEntityAssembler.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Reservation.Model.Entities;
+using Presentation.Reservation.Resources;
+
+namespace Presentation.Reservation.Transform;
+
+public static class ReservationResourceFromEntityAssembler
+{
+    public static ReservationResource ToResourceFromEntity(ReservationEntity entity) =>
+        new ReservationResource(
+            entity.Id,
+            entity.ScooterId,
+            entity.UserId,
+            entity.StartTime,
+            entity.EndTime,
+            entity.CreatedDate
+        );
+}
