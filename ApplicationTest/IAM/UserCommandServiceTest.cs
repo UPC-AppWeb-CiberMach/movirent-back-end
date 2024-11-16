eadb88943dc306e81faba27ac0230c9571e50eb8
Velarde Gonzales Nestor H <u20211c221@upc.edu.pe>
2024-11-16 17:08:01 -0500

    feat: Add the tests for scooter in the application layer

--- ApplicationTest/IAM/UserCommandServiceTest.cs
+++ ApplicationTest/IAM/UserCommandServiceTest.cs
@@ -1,6 +1,36 @@
-﻿namespace ApplicationTest.IAM;
+﻿using Application.IAM.CommandServices;
+using Domain.IAM.Model.Commands;
+using Domain.IAM.Model.Entities;
+using Domain.IAM.Repositories;
+using Domain.Shared;
+using Moq;
+using NSubstitute;
+
+namespace ApplicationTest.IAM;
 
 public class UserCommandServiceTest
 {
-    
+    [Fact]
+    public void CreateUserTest()
+    {
+        // Arrange
+        var usersRepository = new Mock<IUsersRepository>();
+        var unitOfWork = new Mock<IUnitOfWork>();
+        var userCommandService = new UserCommandService(usersRepository.Object, unitOfWork.Object);
+        var command = new CreateUserCommand(
+            "velarde@gmail.com",
+            "8765432112",
+            "terews",
+            "987654321",
+            "87654321",
+            "mifoto",
+            "Los Olivos");
+        
+        // Act
+        var result = userCommandService.Handle(command);
+        
+        // Assert
+        Assert.NotNull(result);
+
+    }
 }
---
+++ ApplicationTest/Renting/ScooterCommandServiceTest.cs
@@ -0,0 +1,142 @@
+﻿using Application.Renting.CommandServices;
+using Domain.Renting.Model.Commands;
+using Domain.Renting.Repositories;
+using Domain.Scooter.Model.Commands;
+using Domain.Shared;
+using Moq;
+
+namespace ApplicationTest.Renting;
+
+public class ScooterCommandServiceTest
+{
+    /*
+     * using Domain.Renting.Model.Commands;
+using Domain.Renting.Model.Entities;
+using Domain.Renting.Repositories;
+using Domain.Renting.Services;
+using Domain.Scooter.Model.Commands;
+using Domain.Shared;
+
+namespace Application.Renting.CommandServices;
+
+public class ScooterCommandService : IScooterCommandService
+{
+    private readonly IScooterRepository _scooterRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ScooterCommandService(IScooterRepository scooterRepository, IUnitOfWork unitOfWork)
+    {
+        _scooterRepository = scooterRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Guid> Handle(CreateScooterCommand command)
+    {
+        var scooter = new ScooterEntity(command);
+        
+        await _scooterRepository.AddAsync(scooter);
+        await _unitOfWork.CompleteAsync();
+        return scooter.Id;
+    }
+
+    public async Task<bool> Handle(UpdateScooterCommand command)
+    {
+        var scooter = await _scooterRepository.GetByIdAsync(command.Id);
+        if (scooter == null)
+        {
+            throw new Exception("Renting not found");
+        }
+        _scooterRepository.Update(scooter);
+        scooter.UpdateScooterInfo(command);
+        await _unitOfWork.CompleteAsync();
+        return true;
+
+    }
+
+    public async Task<bool> Handle(DeleteScooterCommand command)
+    {
+        var scooter = await _scooterRepository.GetByIdAsync(command.Id);
+        if (scooter == null)
+        {
+            throw new Exception("Renting not found");
+        }
+        _scooterRepository.Delete(scooter);
+        await _unitOfWork.CompleteAsync();
+        return true;
+    }
+}
+     */
+    
+    // Deseo que me hagas los Test
+    // para el método Handle(CreateScooterCommand command)
+    // para el método Handle(UpdateScooterCommand command)
+    // para el método Handle(DeleteScooterCommand command)
+    // de la clase ScooterCommandService
+    // con los siguientes casos de prueba:
+
+
+    [Fact]
+    public void CreateScooterTest()
+    {
+        // Arrange
+        var scooterRepository = new Mock<IScooterRepository>();
+        var unitOfWork = new Mock<IUnitOfWork>();
+        var scooterCommandService = new ScooterCommandService(scooterRepository.Object, unitOfWork.Object);
+        var command = new CreateScooterCommand(
+            "Scooter 1",
+            "Scooter 1",
+            "Scooter 1",
+            "Scooter 1",
+            120,
+            "Los Olivos",
+            "987654321");
+
+        // Act
+        var result = scooterCommandService.Handle(command);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+    
+    [Fact]
+    public void UpdateScooterTest()
+    {
+        
+        //public record UpdateScooterCommand(int Id, string Name, string Brand, string Model, string Image, double PricePerHour, string District, string Phone);public record UpdateScooterCommand(int Id, string Name, string Brand, string Model, string Image, double PricePerHour, string District, string Phone);
+        // Arrange
+        var scooterRepository = new Mock<IScooterRepository>();
+        var unitOfWork = new Mock<IUnitOfWork>();
+        var scooterCommandService = new ScooterCommandService(scooterRepository.Object, unitOfWork.Object);
+        var command = new UpdateScooterCommand(
+            1,
+            "Scooter 1",
+            "Scooter 1",
+            "Scooter 1",
+            "Scooter 1",
+            120,
+            "Los Olivos",
+            "987654321");
+
+        // Act
+        var result = scooterCommandService.Handle(command);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+    
+    [Fact]
+    public void DeleteScooterTest()
+    {
+        // Arrange
+        var scooterRepository = new Mock<IScooterRepository>();
+        var unitOfWork = new Mock<IUnitOfWork>();
+        var scooterCommandService = new ScooterCommandService(scooterRepository.Object, unitOfWork.Object);
+        var command = new DeleteScooterCommand(1);
+
+        // Act
+        var result = scooterCommandService.Handle(command);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+}
