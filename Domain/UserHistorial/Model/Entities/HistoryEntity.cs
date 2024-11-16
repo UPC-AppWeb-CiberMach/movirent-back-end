1b6287b20a64dd4595e8ced9e33435e3572bddae
Adriana Diestra <adriana10tra@gmail.com>
2024-11-16 14:47:05 -0500

    add: Context Boud Validation

--- Domain/UserHistorial/Model/Entities/HistoryEntity.cs
+++ Domain/UserHistorial/Model/Entities/HistoryEntity.cs
@@ -5,7 +5,7 @@ public class HistoryEntity
     public long Id { get; set; }
     public Guid ClientId { get; set; }
     public Guid ScooterId { get; set; }
-    
+    //hola
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
     public decimal Price { get; set; }
