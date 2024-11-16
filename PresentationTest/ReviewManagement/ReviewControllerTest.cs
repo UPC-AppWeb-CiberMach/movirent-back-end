7a0634392745aeb01724a42de92492470f002967
Adriana Diestra <adriana10tra@gmail.com>
2024-11-16 10:44:27 -0500

    add: develop the review management test

---
+++ PresentationTest/ReviewManagement/ReviewControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Review.Model.Commands;
+using Domain.Review.Model.Entities;
+using Domain.Review.Model.Queries;
+using Domain.Review.Repositories;
+using Domain.Review.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Presentation.Review.Controllers;
+using Presentation.Review.Resources;
+
+namespace PresentationTest.ReviewManagement;
+
+public class ReviewManagementTest
+{
+    
+    [Fact]
+    public async Task GetReviews()
+    {
+        var reviewCommandService = new Mock<IReviewCommandService>();
+        var reviewQueryService = new Mock<IReviewQueryService>();
+        var query = new GetAllReviewsQuery();
+        var reviews = reviewQueryService.Object.Handle(query).Result;
+        var reviewEntities = reviews as ReviewEntity[] ?? reviews.ToArray();
+        reviewQueryService.Setup(x => x.Handle(query)).ReturnsAsync(reviewEntities);
+        var controller = new ReviewController(reviewQueryService.Object, reviewCommandService.Object);
+        var result = await controller.GetAllScooters() as ObjectResult;
+        Assert.NotNull(result);
+        Assert.Equal(200, result.StatusCode);
+    }
+    
+    
+    [Fact]
+    public async Task CreateReview()
+    {
+        var reviewCommandService = new Mock<IReviewCommandService>();
+        var reviewQueryService = new Mock<IReviewQueryService>();
+        var controller = new ReviewController(reviewQueryService.Object, reviewCommandService.Object);
+        var result = await controller.CreateScooter(new CreateReviewResource(
+            "Test",
+            5
+        )) as ObjectResult;
+        Assert.NotNull(result);
+        Assert.Equal(201, result.StatusCode);
+    }
+    
+}
