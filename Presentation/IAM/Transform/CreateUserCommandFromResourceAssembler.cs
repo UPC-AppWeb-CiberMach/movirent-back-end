3c574e656b27d74e3d053f0477b62ab10690336b
QuiqueVladimir <U202022365@upc.edu.pe>
2024-11-17 08:59:33 -0500

    feat: add HashPassword

--- Presentation/IAM/Transform/CreateUserCommandFromResourceAssembler.cs
+++ Presentation/IAM/Transform/CreateUserCommandFromResourceAssembler.cs
@@ -1,5 +1,8 @@
 ﻿using Domain.IAM.Model.Commands;
 using Presentation.IAM.Resources;
+using System.Security.Cryptography;
+using System.Text;
+
 
 namespace Presentation.IAM.Transform;
 
@@ -7,9 +10,23 @@ public static class CreateUserCommandFromResourceAssembler
 {
     public static CreateUserCommand ToCommandFromResource(CreateUserResource resource)
     {
-        return new CreateUserCommand(resource.email,
-            resource.password, resource.completeName, resource.phone,
-            resource.dni, resource.photo, resource.address);
+        return new CreateUserCommand(
+            resource.email,
+            HashPassword(resource.password), 
+            resource.completeName, 
+            resource.phone,
+            resource.dni, 
+            resource.photo, 
+            resource.address);
+    }
+    
+    private static string HashPassword(string password)
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+        }
     }
 }
 
