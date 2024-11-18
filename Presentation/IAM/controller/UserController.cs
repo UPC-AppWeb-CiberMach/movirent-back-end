f2377aa665cd57c5bd2a2078b90274f8f59a84bd
QuiqueVladimir <U202022365@upc.edu.pe>
2024-11-18 15:59:08 -0500

    feat: update hash password

--- Presentation/IAM/controller/UserController.cs
+++ Presentation/IAM/controller/UserController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Net.Mime;
+using System.Security.Cryptography;
+using System.Text;
 using Domain.IAM.Model.Commands;
 using Domain.IAM.Model.Queries;
+using Domain.IAM.Repositories;
 using Domain.IAM.Services;
 using Microsoft.AspNetCore.Mvc;
 using Presentation.IAM.Resources;
@@ -11,35 +14,45 @@ namespace Presentation.IAM.controller
     [ApiController]
     [Route("api/v1/[controller]")]
     [Produces(MediaTypeNames.Application.Json)]
-    public class UserController (IUserQueryService userQueryService, 
-        IUserCommandService userCommandService): ControllerBase
+    public class UserController : ControllerBase
     {
+        private readonly IUserQueryService _userQueryService;
+        private readonly IUserCommandService _userCommandService;
+        private readonly IUsersRepository _usersRepository;
+
+        public UserController(IUserQueryService userQueryService, IUserCommandService userCommandService, IUsersRepository usersRepository)
+        {
+            _userQueryService = userQueryService;
+            _userCommandService = userCommandService;
+            _usersRepository = usersRepository;
+        }
+
         [HttpGet]
         public async Task<IActionResult> GetAllUsers()
         {
             var query = new GetAllUsersQuery();
-            var users = await userQueryService.Handle(query);
+            var users = await _userQueryService.Handle(query);
             var usersResource = users.Select(UserResourceFromEntityAssembler.ToResourceFromEntity);
             return StatusCode(200, usersResource);
         }
-        
+
         [HttpPost]
         public async Task<IActionResult> CreateUser([FromBody] CreateUserResource userResource)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return StatusCode(400, "Invalid data");
             }
             var command = CreateUserCommandFromResourceAssembler.ToCommandFromResource(userResource);
-            var userId = await userCommandService.Handle(command);
+            var userId = await _userCommandService.Handle(command);
             return StatusCode(201, userId);
         }
-        
+
         [HttpGet("{id:int}")]
         public async Task<IActionResult> GetUserById(int id)
         {
             var query = new GetUsersByIdQuery(id);
-            var user = await userQueryService.Handle(query);
+            var user = await _userQueryService.Handle(query);
             var userResource = UserResourceFromEntityAssembler.ToResourceFromEntity(user);
             if (userResource == null)
             {
@@ -47,21 +60,50 @@ namespace Presentation.IAM.controller
             }
             return StatusCode(200, userResource);
         }
-        
+
         [HttpPut("{id:int}")]
         public async Task<IActionResult> UpdateUser(int id, [FromBody] UpdateUserResource userResource)
         {
             var command = UpdateUserCommandFromResourceAssembler.ToCommandFromResource(id, userResource);
-            await userCommandService.Handle(command);
+            await _userCommandService.Handle(command);
             return StatusCode(200, "User updated");
         }
-        
+
         [HttpDelete("{id:int}")]
         public async Task<IActionResult> DeleteUser(int id)
         {
             var command = new DeleteUserCommand(id);
-            await userCommandService.Handle(command);
-            return StatusCode(200,"User deleted");
+            await _userCommandService.Handle(command);
+            return StatusCode(200, "User deleted");
+        }
+
+        [HttpGet("signin")]
+        public async Task<IActionResult> SignIn([FromQuery] string email, [FromQuery] string password)
+        {
+            var user = await _usersRepository.GetByEmailAsync(email);
+            if (user == null)
+            {
+                return StatusCode(404, "User not found");
+            }
+
+            if (VerifyPassword(password, user.Password))
+            {
+                return StatusCode(200, "Sign in successful");
+            }
+            else
+            {
+                return StatusCode(401, "Invalid password");
+            }
+        }
+
+        private bool VerifyPassword(string plainTextPassword, string hashedPassword)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(plainTextPassword));
+                var hashedInputPassword = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+                return hashedInputPassword == hashedPassword;
+            }
         }
     }
 }
